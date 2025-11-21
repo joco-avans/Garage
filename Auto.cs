57ffea5c0702fca28fc7bea0a2707338b6a785cb
joco-avans <j.corbijn@avans.nl>
2025-11-21 09:26:29 +0100

    First version garage

---
+++ Auto.cs
@@ -0,0 +1,13 @@
+public class Auto
+{
+    public Auto(string merk, string model)
+    {
+        Merk = merk;
+        Model = model;
+    }
+
+    public string Merk {get; init;}
+    public string Model {get; init;}
+
+
+}
